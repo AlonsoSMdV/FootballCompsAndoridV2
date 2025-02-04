1a4bcfc5152c65e0196d1167f39ee61dfff191e6
AlonsoSMdV <alonsosmdv@gmail.com>
2025-02-04 08:55:20 +0100

    Matches backend first version implemented

---
+++ app/src/main/java/com/example/footballcompsuserv2/di/MatchesModule.kt
@@ -0,0 +1,23 @@
+package com.example.footballcompsuserv2.di
+
+import com.example.footballcompsuserv2.data.matches.IMatchRepository
+import com.example.footballcompsuserv2.data.matches.MatchRepository
+import com.example.footballcompsuserv2.data.remote.matches.IMatchesRemoteDataSource
+import com.example.footballcompsuserv2.data.remote.matches.MatchesRemoteDataSource
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class MatchesModule {
+    @Singleton
+    @Binds
+    abstract fun bindMatchesRepository(repository: MatchRepository): IMatchRepository
+
+    @Singleton
+    @Binds
+    abstract fun bindMatchesRemote(remote: MatchesRemoteDataSource): IMatchesRemoteDataSource
+}
