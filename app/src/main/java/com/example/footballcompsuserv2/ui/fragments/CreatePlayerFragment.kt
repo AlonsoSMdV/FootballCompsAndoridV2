a176e0336299d062081ffd35e915820c4c6c16d3
Alonso <asanmor011004@gmail.com>
2025-02-05 19:29:59 +0100

    Player camera and pickMedia implemented

--- app/src/main/java/com/example/footballcompsuserv2/ui/fragments/CreatePlayerFragment.kt
+++ app/src/main/java/com/example/footballcompsuserv2/ui/fragments/CreatePlayerFragment.kt
@@ -1,27 +1,72 @@
 package com.example.footballcompsuserv2.ui.fragments
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.Toast
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
+import coil3.load
 import com.example.footballcompsuserv2.R
 import com.example.footballcompsuserv2.data.remote.players.PlayerCreate
 import com.example.footballcompsuserv2.data.remote.players.PlayerRawAttributes
 import com.example.footballcompsuserv2.databinding.FragmentCreatePlayerBinding
 import com.example.footballcompsuserv2.ui.viewModels.CreatePlayerViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
+private var PERMISSIONS_REQUIRED =
+    arrayOf(
+        Manifest.permission.CAMERA,
+        Manifest.permission.RECORD_AUDIO)
 
 @AndroidEntryPoint
 class CreatePlayerFragment :Fragment(R.layout.fragment_create_player){
     private lateinit var binding: FragmentCreatePlayerBinding
     private val viewModel: CreatePlayerViewModel by viewModels()
     private var idTeam: Int? = null
+
+    private val contract = ActivityResultContracts.RequestMultiplePermissions()
+
+    private val launcher = registerForActivityResult(contract)
+    {
+            permissions ->
+        var granted = true
+        permissions.entries.forEach {
+                permission ->
+            if (permission.key in PERMISSIONS_REQUIRED && !permission.value){
+                granted = false
+            }
+        }
+        if (granted){
+            //Navigate to camera
+            navigateToCamera()
+
+        }else{
+            Toast.makeText(requireContext(), "No tiene permisos de camara", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private val pickMedia = registerForActivityResult(ActivityResultContracts.PickVisualMedia()){ uri ->
+        if (uri != null){
+            viewLifecycleOwner.lifecycleScope.launch {
+                viewModel.onImageCaptured(uri)
+            }
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -36,14 +81,61 @@ class CreatePlayerFragment :Fragment(R.layout.fragment_create_player){
         return binding.root
     }
 
+    /**
+     * Función que navega al fragmento de Preview de camara
+     */
+    private fun navigateToCamera() {
+        val action = CreatePlayerFragmentDirections.createPlayerToCamera()
+        findNavController().navigate(action)
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.playerToCamera.setOnClickListener{
+            if(hasCameraPermissions(requireContext())){
+                // navigateToCamera()
+
+            }else {
+                launcher.launch(PERMISSIONS_REQUIRED)
+            }
+        }
 
         binding.createPlayerToolbar.apply {
             setNavigationOnClickListener {
                 findNavController().navigateUp()
             }
         }
+
+        binding.playerSelectPhotoBtn.setOnClickListener {
+            pickMedia.launch(PickVisualMediaRequest((ActivityResultContracts.PickVisualMedia.ImageOnly)))
+        }
+
+        val btnCamera = view.findViewById<Button>(R.id.player_to_camera)
+        btnCamera.setOnClickListener {
+            if (hasCameraPermissions(requireContext())){
+                navigateToCamera()
+            }else{
+                launcher.launch(PERMISSIONS_REQUIRED)
+            }
+        }
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED){
+                viewModel.photo.collect{
+                        photoUri ->
+                    when(photoUri){
+                        Uri.EMPTY -> {
+                            //No hay foto, podemos poner un placeholder
+                        }
+                        else -> {
+                            //tenemos la foto, la ponemos en la UI aprovechando coil
+                            binding.playerImageView.load(photoUri)
+                        }
+                    }
+                }
+            }
+        }
+
 
 
         val btnCreate = view.findViewById<Button>(R.id.create_player)
@@ -77,6 +169,14 @@ class CreatePlayerFragment :Fragment(R.layout.fragment_create_player){
                 viewModel.createPlayer(createPlayer)
                 findNavController().navigate(R.id.create_to_players)
             }
+        }
+    }
+    private fun hasCameraPermissions(context: Context):Boolean {
+        return PERMISSIONS_REQUIRED.all { permission ->
+            ContextCompat.checkSelfPermission(
+                context,
+                permission
+            ) == PackageManager.PERMISSION_GRANTED
         }
     }
 }
