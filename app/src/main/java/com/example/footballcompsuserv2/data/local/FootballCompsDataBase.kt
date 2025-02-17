da53f0f8f74ea63030be46eba06e203a6851ad19
Alonso <asanmor011004@gmail.com>
2025-02-17 19:20:04 +0100

    FootballCompsDatabase implemented

--- app/src/main/java/com/example/footballcompsuserv2/data/local/FootballCompsDataBase.kt
+++ app/src/main/java/com/example/footballcompsuserv2/data/local/FootballCompsDataBase.kt
@@ -1,4 +1,52 @@
 package com.example.footballcompsuserv2.data.local
 
-class FootballCompsDataBase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.footballcompsuserv2.data.local.dao.LeagueDao
+import com.example.footballcompsuserv2.data.local.dao.MatchDao
+import com.example.footballcompsuserv2.data.local.dao.PlayerDao
+import com.example.footballcompsuserv2.data.local.dao.TeamDao
+import com.example.footballcompsuserv2.data.local.dao.UserDao
+import com.example.footballcompsuserv2.data.local.entities.LeagueEntity
+import com.example.footballcompsuserv2.data.local.entities.MatchEntity
+import com.example.footballcompsuserv2.data.local.entities.PlayerEntity
+import com.example.footballcompsuserv2.data.local.entities.TeamEntity
+import com.example.footballcompsuserv2.data.local.entities.UserEntity
+
+@Database(entities = [LeagueEntity::class,
+                      TeamEntity::class,
+                      PlayerEntity::class,
+                      MatchEntity::class,
+                      UserEntity::class],
+                      version = 1)
+abstract class FootballCompsDataBase: RoomDatabase() {
+
+    abstract fun leagueDao(): LeagueDao
+    abstract fun teamDao(): TeamDao
+    abstract fun playerDao(): PlayerDao
+    abstract fun matchDao(): MatchDao
+    abstract fun userDao(): UserDao
+
+    companion object{
+        @Volatile
+        private var _INSTANCE: FootballCompsDataBase? = null
+
+        fun getInstance(context: Context): FootballCompsDataBase{
+            return _INSTANCE ?: synchronized(this){
+                _INSTANCE ?: buildDatabase(context).also{
+                    database -> _INSTANCE = database
+                }
+            }
+        }
+
+        private fun buildDatabase(context: Context): FootballCompsDataBase{
+            return  Room.databaseBuilder(
+                    context.applicationContext,
+                    FootballCompsDataBase::class.java,
+                    "footballcomps_db"
+                    ).build()
+        }
+    }
 }
