e210481843a2966e89750f8ab62a1d5bbeb6e606
AlonsoSMdV <alonsosmdv@gmail.com>
2025-02-19 14:12:01 +0100

    Match local works correctly

--- app/src/main/java/com/example/footballcompsuserv2/data/matches/MatchRepository.kt
+++ app/src/main/java/com/example/footballcompsuserv2/data/matches/MatchRepository.kt
@@ -1,31 +1,46 @@
 package com.example.footballcompsuserv2.data.matches
 
+import android.content.Context
+import com.example.footballcompsuserv2.data.local.ILocalDataSource
 import com.example.footballcompsuserv2.data.remote.matches.IMatchesRemoteDataSource
+import com.example.footballcompsuserv2.di.NetworkUtils
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
 class MatchRepository @Inject constructor(
-    val remote: IMatchesRemoteDataSource
-): IMatchRepository{
+    private val remote: IMatchesRemoteDataSource,
+    private val local: ILocalDataSource, // Agregamos la fuente de datos local
+    private val networkUtils: NetworkUtils
+) : IMatchRepository {
+
     private val _state = MutableStateFlow<List<Match>>(listOf())
     override val setStream: StateFlow<List<Match>>
         get() = _state.asStateFlow()
 
     override suspend fun readAll(): List<Match> {
-        val res = remote.readAll()
-        val matches = _state.value.toMutableList()
+        val matches = mutableListOf<Match>()
+        if (networkUtils.isNetworkAvailable()) {
+            //  Si hay internet, obtener datos del servidor
+            val res = remote.readAll()
+            if (res.isSuccessful) {
+                val matchList = res.body()?.data ?: emptyList()
+                _state.value = matchList.toExternal()
 
-        if (res.isSuccessful){
-            val matchList = res.body()?.data ?: emptyList()
-            matchList.forEach { match ->
-                println("Local ID: ${match.attributes.local}")
-                println("Visitor ID: ${match.attributes.visitor}")
+                //  Guardar los datos en la base de datos local para uso offline
+                matchList.forEach { match ->
+                    local.createLocalMatch(match.toExternal().toLocal())
+                }
+
+                return matchList.toExternal()
             }
-            _state.value = matchList.toExternal()
         }
-        else _state.value = matches
+        //  Si no hay internet, cargar datos locales
+        local.getLocalMatches().collect { localMatches ->
+            _state.value = localMatches.localToExternal()
+        }
+
         return matches
     }
 }
